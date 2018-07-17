85e28289e230f58d5c53bc3a6075c4808143d0b4
TerrellWhite <Terrell.White053100@gmail.com>
2018-07-17 11:58:30 -0400

    Folder full of the Project files

---
+++ Grand_Lab2/Grand_Lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grand_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args) // Program finds the area and perimeter of a room through prompt
+        {
+            double Length, Width, Area, Perimeter, Height,Volume;
+            bool userInput;
+            do
+            {
+                Console.WriteLine("Enter Length:");
+                Length = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Width:");
+                Width = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Height:");
+                Height = double.Parse(Console.ReadLine());
+                Area = Length * Width;
+                Perimeter = 2 * (Length + Width);
+                Volume = Length * Width * Height;
+                Console.WriteLine($"The area is {Area}");
+                Console.WriteLine($"The Perimeter is {Perimeter}");
+                Console.WriteLine($"The Volume is {Volume}");
+                Console.WriteLine("Go Again? (true/false)");
+                userInput = Convert.ToBoolean(Console.ReadLine());
+            } while (userInput == true);
+        }
+    }
+}
